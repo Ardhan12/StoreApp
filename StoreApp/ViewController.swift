7b563ea7ea299f14ba41110788f510a15cb262b6
Ardhan12 <arief.ramadhan43@gmail.com>
2023-01-03 08:16:19 +0700

    Initial Commit

---
+++ StoreApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  StoreApp
+//
+//  Created by Arief Ramadhan on 03/01/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
